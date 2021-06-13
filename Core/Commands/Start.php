e0657c45d3eb7580a28f6eb0cd4a904d0e202a3a
gizmore <gizmore@wechall.net>
2021-06-13 17:25:47 +0200

    6.10.4-r1167_dog

--- Core/Commands/Start.php
+++ Core/Commands/Start.php
@@ -5,7 +5,7 @@ use GDO\Dog\DOG_Command;
 
 final class Start extends DOG_Command
 {
-    public $group = 'Shadowdogs';
+    public $group = 'sd';
     public $trigger = "start";
     
 }
--- Shadowcommand.php
+++ Shadowcommand.php
@@ -5,7 +5,7 @@ use GDO\Dog\DOG_Command;
 
 abstract class Shadowcommand extends DOG_Command
 {
-    public $group = 'Shadowdogs';
+    public $group = 'sd';
     
     public function isLeaderCommand() { return false; }
     
