f4d8a3d58bd37be1fdcb482b8ae9aa0e325dc765
gizmore <gizmore@wechall.net>
2023-03-27 20:11:12 +0200

    '7.0.3-r1'

--- Method/Command.php
+++ Method/Command.php
@@ -1,11 +1,16 @@
 <?php
 namespace GDO\DogShadowdogs\Method;
 
+use GDO\Core\GDT;
+use GDO\Core\GDT_Response;
 use GDO\Core\Method;
 
 final class Command extends Method
 {
 
-	public function execute() {}
+	public function execute(): GDT
+	{
+		return GDT_Response::make();
+	}
 
 }
